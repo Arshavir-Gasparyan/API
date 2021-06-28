473dc2a49a8237e2e5e6a791f7c93aed396bb71a
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-06-28 18:52:16 +0400

    fix: solve the problem with fetch

--- filmApi/script.js
+++ filmApi/script.js
@@ -1,37 +1,65 @@
-
 let baseUrl = "https://ghibliapi.herokuapp.com/films/";
-const select = document.querySelector("select");
-const title = document.getElementById("title")
-const description = document.getElementById("description")
-const director = document.getElementById("director")
-const producer = document.getElementById("producer")
-const release_date = document.getElementById("release_date")
-let id;
-const fetchAllFilm = ()=>{
-    fetch(`${baseUrl}`).then((res)=>res.json()).then((data)=>{
-        for(let item of data){
-            let option = document.createElement("option");
-            option.innerText = item.title;
-            select.appendChild(option)
-               if (item.title===select.value) {
-                   id = item.id
-               }   
-            }   
-    })
-}
-fetchAllFilm()
-const fetchOneFilm = ()=>{
+const select = document.getElementById("sel");
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const director = document.getElementById("director");
+const producer = document.getElementById("producer");
+const releaseDate = document.getElementById("release_date");
+let allFilmsData;
 
-    fetch(`${baseUrl}${id}`).then((res)=>res.json()).then((data)=>{
-        title.innerText = data.title
-        description.innerText=data.description
-        director.innerText=data.director
-        producer.innerText=data.producer
-        release_date.innerText=data.release_date
-    }) 
-    fetchAllFilm();
-}
+const getFilmsData = () => {
+  fetch("https://ghibliapi.herokuapp.com/films")
+    .then((res) => res.json())
+    .then((data) => {
+      allFilmsData = data;
+      allFilmsData.forEach((item) => {
+        let option = document.createElement("option");
+        option.innerText = item.title;
+        option.value = item.id;
+        select.appendChild(option);
+      });
+    });
+};
+getFilmsData();
+
+const fillFilmData = (filmData) => {
+  title.innerText = filmData.title;
+  description.innerText = filmData.description;
+  director.innerText = filmData.director;
+  producer.innerText = filmData.producer;
+  releaseDate.innerText = filmData.release_date;
+};
 
 select.addEventListener("change", () => {
-    fetchOneFilm(id)
-  });
+  let film = allFilmsData.find((item) => item.id === select.value);
+  fillFilmData(film);
+});
+
+// const fetchAllFilm = () => {
+//   fetch(`${baseUrl}`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       for (let item of data) {
+//         let option = document.createElement("option");
+//         option.innerText = item.title;
+//         select.appendChild(option);
+//         // if (item.title === select.value) {
+//         //   id = item.id;
+//         // }
+//       }
+//     });
+// };
+// fetchAllFilm();
+// const fetchOneFilm = () => {
+//   title.innerText = AllFilms.title;
+//   description.innerText = AllFilms.description;
+//   director.innerText = AllFilms.director;
+//   producer.innerText = AllFilms.producer;
+//   release_date.innerText = AllFilms.release_date;
+
+//   fetchAllFilm();
+// };
+
+// select.addEventListener("change", () => {
+//   fetchOneFilm();
+// });
